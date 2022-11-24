9387992b7e5af60c4258fcbd91e4af9d09eec909
dalessandro.ambrogio <dalessandro.ambrogio@accenture.com>
2022-11-24 16:15:46 +0100

    feat login: config webview

--- app/src/main/java/com/example/igeniusandroidtest/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/igeniusandroidtest/ui/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.igeniusandroidtest.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +21,14 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.webview.apply {
+            webViewClient = WebViewClient()
+            loadUrl("https://github.com/login")
+        }
     }
 
     override fun onDestroyView() {
