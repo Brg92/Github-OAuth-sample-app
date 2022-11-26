290d6c1fd53fb1ed482f58dba06d92e30a1079c0
dalessandro.ambrogio <dalessandro.ambrogio@accenture.com>
2022-11-26 18:44:45 +0100

    feat Loading: implement animation to logo
    complete navigation graph

--- app/src/main/java/com/example/igeniusandroidtest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/igeniusandroidtest/ui/home/HomeFragment.kt
@@ -5,12 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.igeniusandroidtest.databinding.FragmentHomeBinding
 import com.example.igeniusandroidtest.databinding.FragmentLoadingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-    private var _binding: FragmentLoadingBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -18,7 +19,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoadingBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
--- app/src/main/java/com/example/igeniusandroidtest/ui/loading/LoadingFragment.kt
+++ app/src/main/java/com/example/igeniusandroidtest/ui/loading/LoadingFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.igeniusandroidtest.R
 import com.example.igeniusandroidtest.databinding.FragmentLoadingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoadingFragment : Fragment() {
     private var _binding: FragmentLoadingBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<LoadingViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,38 @@ class LoadingFragment : Fragment() {
     ): View {
         _binding = FragmentLoadingBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO: animations should end once the api call is completed
+        binding.motionLayout.setTransitionListener(object : TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {}
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {}
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                findNavController().navigate(R.id.go_to_home)
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {}
+        })
+
     }
 
     override fun onDestroyView() {
